cdf2b398b7b4721e40480dff7721f6cae9f2dafd
sheetal <sheetal@sheetals-MacBook-Pro.local>
2020-11-05 10:40:33 -0800

    preparing for github pages

--- main.js
+++ main.js
@@ -5,6 +5,85 @@ var tableCell = document.getElementsByTagName("td");
 var tableSlot = document.querySelector(".slot");
 const playerTurn = document.querySelector(".player-turn");
 const reset = document.querySelector(".reset");
+let winCombos = [
+    [0,1,2,3],[1,2,3,4],[2,3,4,5],[3,4,5,6],
+    [7,8,9,10],[8,9,10,11],[9,10,11,12],[10,11,12,13],
+    [14,15,16,17],[15,16,17,18],[16,17,18,19],[17,18,19,20],
+    [21,22,23,24],[22,23,24,25],[23,24,25,26],[24,25,26,27],
+    [28,29,30,31],[29,30,31,32],[30,31,32,33],[31,32,33,34],
+    [35,36,37,38],[36,37,38,39],[37,38,39,40],[38,39,40,41],
+
+    //Adding winning logic vertically
+
+[0,7,14,21],[7,14,21,28],[14,21,28,35],
+[1,8,15,22],[8,15,22,29],[15,22,29,36],
+[2,9,16,23],[9,16,23,30],[16,23,30,37],
+[3,10,17,24],[10,17,24,31],[17,24,31,38],
+[4,11,18,25],[11,18,25,32],[18,25,32,39],
+[5,12,19,26],[12,19,26,33],[19,26,33,40]
+[6,13,20,27],[13,20,27,34],[20,27,34,41],
+
+    //Adding winning logic bottoms up
+
+    [35,28,21,14],[28,21,14,7],[21,14,7,0],
+    [36,29,22,15],[29,22,15,8],[22,15,8,1],
+    [37,30,23,16],[30,23,16,9],[23,16,9,2],
+    [38,31,24,17],[31,24,17,10],[24,17,10,3],
+    [39,32,25,18],[32,25,18,11],[25,18,11,4],
+    [40,33,26,19],[33,26,19,12],[26,19,12,5],
+    [41,34,27,20],[34,27,20,13],[27,20,13,6],
+
+    //Adding winning logic right to left horizontal
+
+    [6,5,4,3],[5,4,3,2],[4,3,2,1],[3,2,1,0],
+    [13,12,11,10],[12,11,10,9],[11,10,9,8],[10,9,8,7],
+    [20,19,18,17],[19,18,17,16],[18,17,16,15,],[17,17,15,14],
+    [27,26,25,24],[26,25,24,23],[25,24,23,22],[24,23,22,21],
+    [34,33,32,31],[33,32,31,30],[32,31,30,29],[31,30,29,28],
+    [41,40,39,38],[40,39,38,37],[39,38,37,36],[38,37,36,35]
+
+
+
+
+]
+
+//diagonal left and diagomal right
+
+//create a function to make sure color matches
+
+function colorMatchCheck(one,two,three,four){
+    return(one ==two && one ===three && one ===four && one !=='white');
+}
+
+function horizontalcheck{
+    for(let row = 0;row <tableRow.length; row++){
+        for(let col = 0; col <4 ; col++{
+            if(colorMatchCheck(tableRow))
+
+        }
+    }
+
+}
+function verticalCheck{
+    for (let col = 0; col < 7; col++){
+        for (row = 0; row <3; row++){
+            if(colorMatchCheck(tableRow[row].col.style.backgroundColor))
+
+        }
+    }
+
+}
+
+ function diagonalCheckLeft{
+
+ }
+  function diagonalCheckRight{
+
+  }
+
+
+
+
 
 for (let i = 0; i < tableCell.length; i++) {
   tableCell[i].addEventListener("click", (e) => {
@@ -17,6 +96,7 @@ for (let i = 0; i < tableCell.length; i++) {
 // while (!player1) {
 //   var player1 = prompt("player 1: Enter your name.You will be red");
 // }
+  //need players and gave them colors
 
 let player1color = "red";
 let player1 = "myself";
@@ -40,30 +120,32 @@ Array.prototype.forEach.call(tableCell, (cell) => {
   cell.style.backgroundColor = "white"; //cell background is white so thats why we need this method here
 });
 
-
-
 function changeColor(e) {
   //in this function, we will create a variable called  column  and it will take an event and e is an event
   console.log(e.target);
-  let column = e.target.cellIndex; // whenever we click on any of these cells, it will take the actual column index for eg: 1st index or 2,3
+  let column = e.target.cellIndex;            // whenever we click on any of these cells, it will take the actual column index for eg: 1st index or 2,3
 //  let cellNum = column.split('')[column.length-1];
   console.log(column);
-  let row = [];
+  let row = [];                 //empty state
 
   for (let i = 5; i > -1; i--) {
     console.log(tableRow[i].cells[column]);
-    //why -ve coz we want the loop to check the bottom first and then go up -ve.
+    //we have -ve loop because we want the loop to check the bottom first and then go up .
     if (tableRow[i].cells[column].style.backgroundColor == "white") {
       //-1 means no
       row.push(tableRow[i].cells[column]);
       if (currentPlayer == 1) {
         row[0].style.backgroundColor = player1color;
         currentPlayer =2;
+        playerTurn.textContent = `${player2}'s turn!`;
+        
         break;
+    
       }
       if(currentPlayer == 2){
         row[0].style.backgroundColor = player2color;
         currentPlayer = 1;
+        playerTurn.textContent = `${player1}'s turn!`;
         break;
 
       }
@@ -71,11 +153,14 @@ function changeColor(e) {
   }
 }
 
+
+
+
 reset.addEventListener('click',() =>{
     tableSlot.forEach(slot =>{
         slot.style.background = 'white';
     });
-    playerTurn.style.backgroundColor = 'black';
+    playerTurn.style.Color = 'black';
     return(currentPlayer ===1 ? playerTurn.textContent = `${player}'s turn ` : playerTurn.textContent = `${player2}'s turn`);
     })
 
